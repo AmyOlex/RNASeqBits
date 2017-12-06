4400af30f549877f9f859b4e26ef81374e76991d
Amy Olex <alolex@vcu.edu>
2017-12-06 13:56:24 -0500

    Added new method to create a centered color palette at zero based on a datasets range.

---
+++ R/center.palette.R
@@ -0,0 +1,29 @@
+#' Calculates a color palette where zero values are centered at white.
+#'
+#' @param data A matrix of integers.
+#' @param palette_length The size of the color palette. Default = 100
+#' @param color1 The color for the low bound. Default = "blue"
+#' @param color2 The upper bound color.  Default = "red"
+#' @keywords color, heatmap, palette
+#' @return A vector containing the new color palette.
+#' @examples
+#' my_palette <- center.palette(data, palette_length = 200, color1 = "black", color2 = "pink")
+#' @export center.palette
+#' @author Amy L. Olex \email{alolex@@vcu.edu}
+#'
+center.palette <- function(data, palette_length = 100, color1 = "blue", color2 = "red"){
+
+  my_range <- range(data)
+  if((my_range[1] >= 0) | (my_range[2] <= 0)){
+    stop("Range does not cross zero. Cannot center color palette.")
+  }
+
+  diff <- my_range[2] - my_range[1]
+  length1 <- floor((abs(my_range[1])/diff)*palette_length)
+  length2 <- floor((abs(my_range[2])/diff)*palette_length)
+
+
+  color1=colorRampPalette(colors=c(color1, "white"))(length1)
+  color2=colorRampPalette(colors=c("white", color2))(length2)
+  return(c(color1, color2))
+}
